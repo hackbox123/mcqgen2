2be3c8a7ae0343632d6fe364ae57bfff9fef9854
ABHISHEK KUMAR <ak6041574@gmail.com>
2024-07-30 20:02:44 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="MCQ Generatot",
+    version="0.0.0",
+    author="Abhishek kumar",
+    author_email="Ak6041574@gmail.com",
+    packages=find_packages(),
+    install_reqires=[]
+)
